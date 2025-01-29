4f9a2b0b08cc0323b5bc6724863d0dd22124f2d8
Esanta254 <santaemmanuel2018@gmail.com>
2025-01-29 18:56:49 +0300

    Fix serverless configuration for Vercel

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
+# from app.create_app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
 from app.create_app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+if __name__ == "__main__":
+    # Do not use app.run() in serverless environments
+    pass
 
 
 
